08deece4447dd0d5afeb5ef602bdb63bf53ad653
Newgate <theaayushdev07@gmail.com>
2024-01-18 09:55:48 +0545

    first commit

---
+++ os.c
@@ -0,0 +1,50 @@
+// Program of FCFS Scheduling :
+
+#include<stdio.h> 
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) 
+{ 
+    
+    wt[0] = 0;                     // waiting time for first process is 0 
+     
+    for (int  i = 1; i < n ; i++ ) // calculating waiting time
+        wt[i] =  bt[i-1] + wt[i-1] ; 
+}
+   // Function to calculate turn around time 
+void findTurnAroundTime( int processes[], int n,  int bt[], int wt[], int tat[])
+{ 
+    for (int  i = 0; i < n ; i++)                          
+        tat[i] = bt[i] + wt[i]; 
+}    
+void findavgTime( int processes[], int n, int bt[])     	//Function to calculate average time 
+{ 
+    int wt[n], tat[n], total_wt = 0, total_tat = 0; 
+    findWaitingTime(processes, n, bt, wt);		//Function to find waiting time of all processes
+  findTurnAroundTime(processes, n, bt, wt, tat);      //Function to find turn around time for all processes 
+  printf("Processes   Burst time   Waiting time   Turn around time\n");    
+  for (int  i=0; i<n; i++)           // Calculate total waiting time and total turn around time
+    { 
+        total_wt = total_wt + wt[i]; 
+        total_tat = total_tat + tat[i]; 
+        printf("       %d ",(i+1));
+        printf("       %d ", bt[i] );
+        printf("       %d",wt[i] );
+        printf("                 %d\n",tat[i] ); 
+    } 
+    float s=(float)total_wt / (float)n;
+    float t=(float)total_tat / (float)n;
+    printf("Average waiting time = %f",s);
+    printf("\n");
+    printf("Average turn around time = %f ",t); 
+} 
+   // Driver code 
+int main() 
+{ 
+    //process id's 
+    int processes[] = { 1, 2, 3}; 
+    int n = sizeof (processes) / sizeof (processes[0]); 
+   
+    //Burst time of all processes 
+    int  burst_time[] = {10, 5, 8};
+    findavgTime(processes, n,  burst_time); 
+    return 0; 
+}
